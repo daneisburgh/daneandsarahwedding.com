8a922dc102b504ef68517a4c73963281a0e8fa3b
Dane Isburgh <daneisburgh@gmail.com>
2020-04-18 11:44:07 -0400

    Update sequelize constructor

--- backend/src/database/sequelize.ts
+++ backend/src/database/sequelize.ts
@@ -3,7 +3,10 @@ import { Sequelize } from 'sequelize';
 import config from './config';
 const params = config[process.env.NODE_ENV as string];
 
-export = new Sequelize(params.database, params.username, params.password, {
+export = new Sequelize({
+    dialect: 'postgres',
     host: params.host,
-    dialect: 'postgres'
-});
+    database: params.database,
+    username: params.username,
+    password: params.password
+})
