7d4e0a86c5c3e188bab6a16274ec0d18e149a709
Dane Isburgh <daneisburgh@gmail.com>
2020-04-20 19:47:23 -0400

    Remove database name, use default database

--- backend/src/database/config.ts
+++ backend/src/database/config.ts
@@ -1,7 +1,7 @@
 export = {
     [process.env.NODE_ENV as string]: {
         dialect: 'postgres',
-        database: process.env.APP_NAME as string,
+        database: 'postgres',
         host: process.env.DB_HOST as string,
         username: process.env.DB_USERNAME as string,
         password: process.env.DB_PASSWORD as string
