db55b8b5ee0af8913b979ec12119323790a575ea
daneisburgh <daneisburgh@gmail.com>
2020-04-21 00:51:43 -0400

    Update admin username and max guests

--- backend/src/database/data/admin.ts
+++ backend/src/database/data/admin.ts
@@ -2,10 +2,10 @@ import User from '../models/user';
 
 export = async function () {
     await User.create({
-        username: 'DSI1',
+        username: 'D&S',
         name: 'Dane & Sarah Isburgh',
         address: '7907 Ramble View #304, Cincinnati OH, 45231',
-        maxGuests: 0,
+        maxGuests: 2,
         isAdmin: true
     });
 }
