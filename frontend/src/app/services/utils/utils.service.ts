8f71183d1ca7cff4192861c514b331c7bdcbc00c
Dane Isburgh <daneisburgh@gmail.com>
2020-04-22 13:52:45 -0400

    Add header to toast options

--- frontend/src/app/services/utils/utils.service.ts
+++ frontend/src/app/services/utils/utils.service.ts
@@ -46,12 +46,13 @@ export class UtilsService {
         return `/gallery/${path}`;
     }
 
-    public async presentToast(color: string, message: string) {
+    public async presentToast(color: string, header: string, message?: string) {
         const toastOptions: ToastOptions = {
             color,
+            header,
             message,
             position: 'top',
-            duration: 6000
+            duration: 60000
         };
 
         (await this.toastController.create(toastOptions)).present();
