7b7dbd9861bdb14a0c474822db740c0407b3b3bf
Dane Isburgh <daneisburgh@gmail.com>
2020-04-22 13:49:01 -0400

    Add create/verify token helpers to route utils

--- backend/src/api/utils.ts
+++ backend/src/api/utils.ts
@@ -1,9 +1,26 @@
+import jwt from 'jsonwebtoken';
 import { get } from 'lodash';
+
+const API_KEY = process.env.API_KEY as string;
+
+interface Decoded {
+    username: string;
+    iat: number;
+    exp: number;
+}
 
 export function getRequestBody(event: object) {
     return JSON.parse(get(event, 'body'));
 }
 
-export function response(statusCode: number, body?: any) {
+export function createResponse(statusCode: number, body?: any) {
     return { statusCode, body: JSON.stringify(body) };
+}
+
+export function createToken(username: string) {
+    return jwt.sign({ username }, API_KEY, { expiresIn: '1 week' });
+}
+
+export function verifyToken(token: string) {
+    return jwt.verify(token, API_KEY) as Decoded;
 }
