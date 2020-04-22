56f957023910f8204adfe28e36064cc8e01230fd
Dane Isburgh <daneisburgh@gmail.com>
2020-04-22 13:48:38 -0400

    Add user update route

---
+++ backend/src/api/routes/update.ts
@@ -0,0 +1,38 @@
+import { pick } from 'lodash';
+
+import { createResponse, getRequestBody, verifyToken } from '../utils';
+import User from '../../database/models/user';
+
+export default async function (event: any) {
+    try {
+        const { token, user } = getRequestBody(event);
+        let username: string;
+
+        try {
+            const decoded = verifyToken(token);
+            username = decoded.username;
+        } catch {
+            return createResponse(401, 'Invalid Token');
+        }
+
+        if (!username) {
+            return createResponse(401, 'Invalid Username');
+        }
+
+        const response = await User.update(pick(user, [
+            'guests',
+            'isAttending',
+            'requiresAccommodations',
+            'totalRequiredRooms',
+            'requiresTransportation'
+        ]), { where: { username } });
+
+        console.log(response);
+
+        return createResponse(200);
+    } catch (error) {
+        console.error(error);
+        return createResponse(500);
+    }
+}
+
