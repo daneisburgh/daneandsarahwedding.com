0d95737efbee141a16bb0da5eebc9dfbfa07378a
Dane Isburgh <daneisburgh@gmail.com>
2020-04-22 13:50:30 -0400

    Rename and add user columns

--- backend/src/database/models/user.ts
+++ backend/src/database/models/user.ts
@@ -29,7 +29,7 @@ export const userColumns = {
         type: DataTypes.STRING,
         allowNull: true
     },
-    passwordResetExpiration: {
+    passwordResetTokenExpiration: {
         type: DataTypes.DATE,
         allowNull: true
     },
@@ -42,6 +42,10 @@ export const userColumns = {
     },
     emailConfirmationToken: {
         type: DataTypes.STRING,
+        allowNull: true
+    },
+    emailConfirmationTokenExpiration: {
+        type: DataTypes.DATE,
         allowNull: true
     },
     isEmailConfirmed: {
@@ -64,23 +68,19 @@ export const userColumns = {
     },
     isAttending: {
         type: DataTypes.BOOLEAN,
-        defaultValue: false,
-        allowNull: false
+        allowNull: true
     },
-    needsAccommodation: {
+    requiresAccommodations: {
         type: DataTypes.BOOLEAN,
-        defaultValue: false,
-        allowNull: false
+        allowNull: true
     },
-    totalRooms: {
+    totalRequiredRooms: {
         type: DataTypes.INTEGER,
-        defaultValue: 0,
-        allowNull: false
+        allowNull: true
     },
-    needsTransportation: {
+    requiresTransportation: {
         type: DataTypes.BOOLEAN,
-        defaultValue: false,
-        allowNull: false
+        allowNull: true
     },
     createdAt: {
         type: DataTypes.DATE,
@@ -101,17 +101,18 @@ class User extends Model {
     public password!: string;
     public isPasswordHashed!: boolean;
     public passwordResetToken!: string;
-    public passwordResetExpiration!: Date;
+    public passwordResetTokenExpiration!: Date;
     public email!: string;
     public emailConfirmationToken!: string;
+    public emailConfirmationTokenExpiration!: Date;
     public isEmailConfirmed!: boolean;
     public guests!: object;
     public maxGuests!: number;
     public isAdmin!: boolean;
     public isAttending!: boolean;
-    public needsAccommodation!: boolean;
-    public totalRooms!: boolean;
-    public needsTransportation!: boolean;
+    public requiresAccommodations!: boolean;
+    public totalRequiredRooms!: boolean;
+    public requiresTransportation!: boolean;
     public createdAt!: Date;
     public updatedAt!: Date;
 }
@@ -125,11 +126,12 @@ User.beforeCreate(async (user: User) => {
     user.password = Math.random().toString(36).slice(2).substring(0, 5).toUpperCase();
 });
 
-User.afterUpdate(async (user: User) => {
+User.beforeUpdate(async (user: User) => {
     user.updatedAt = new Date;
     
     if (user.changed('password')) {
         user.password = bcrypt.hashSync(user.password, 10);
+        user.isPasswordHashed = true;
     }
 });
 
