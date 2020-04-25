39c9cecb2d08c5c6a3e09665381973282bbd88af
daneisburgh <daneisburgh@gmail.com>
2020-04-25 15:30:36 -0400

    Add authenticate and initialize app test

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,9 +1,9 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { ComponentFixture, TestBed, async, inject } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
 
-import { Platform, PopoverController, ModalController } from '@ionic/angular';
+import { Platform, PopoverController, ModalController, IonicModule } from '@ionic/angular';
 import { IonicStorageModule } from '@ionic/storage';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -12,74 +12,52 @@ import { AppComponent } from './app.component';
 import { UserService } from './services/user/user.service';
 
 describe('AppComponent', () => {
-    let fixture: ComponentFixture<AppComponent>;
-    let component: AppComponent;
-
-    let modalControllerSpy: any;
-    let popoverControllerSpy: any;
-    let statusBarSpy: any;
-    let splashScreenSpy: any;
-    let platformReadySpy: any;
-    let userServiceLogInSpy: any;
-
-    let userService: UserService;
-    let userServiceSpy: any;
-
-    beforeEach(async(() => {
-        modalControllerSpy = jasmine.createSpyObj('ModalController', ['create']);
-        popoverControllerSpy = jasmine.createSpyObj('PopoverController', ['create']);
-        statusBarSpy = jasmine.createSpyObj('StatusBar', ['styleDefault']);
-        splashScreenSpy = jasmine.createSpyObj('SplashScreen', ['hide']);
-
-        const platformSpy = jasmine.createSpyObj('Platform', ['ready']);
-        platformReadySpy = platformSpy.ready.and.returnValue(Promise.resolve());
-
-        // const userServiceSpy = jasmine.createSpyObj('UserService', ['logIn']);
-        // userServiceLogInSpy = userServiceSpy.logIn.and.returnValue(Promise.reject());
-
-        const userService = TestBed.inject(UserService);
-        userServiceSpy = spyOnAllFunctions(userService);
-
+    beforeEach(async () => {
         TestBed.configureTestingModule({
             declarations: [AppComponent],
             schemas: [CUSTOM_ELEMENTS_SCHEMA],
-            providers: [
-                { provide: ModalController, useValue: modalControllerSpy },
-                { provide: PopoverController, useValue: popoverControllerSpy },
-                { provide: StatusBar, useValue: statusBarSpy },
-                { provide: SplashScreen, useValue: splashScreenSpy },
-                { provide: Platform, useValue: platformSpy },
-                UserService
-            ],
             imports: [
                 HttpClientModule,
+                IonicModule.forRoot(),
                 IonicStorageModule.forRoot(),
                 RouterTestingModule.withRoutes([])
             ],
+            providers: [
+                ModalController,
+                PopoverController,
+                StatusBar,
+                SplashScreen,
+                Platform,
+                UserService
+            ]
         }).compileComponents();
-
-        fixture = TestBed.createComponent(AppComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    }));
-
-    it('should create and initialize the app', async () => {
-        expect(component).toBeTruthy();
-        expect(userServiceSpy.logIn).toHaveBeenCalled();
-        expect(platformReadySpy).toHaveBeenCalled();
-        expect(statusBarSpy.styleDefault).toHaveBeenCalled();
-        expect(splashScreenSpy.hide).toHaveBeenCalled();
     });
 
-    // it('should initialize the app', async(inject([UserService], (userService: UserService) => {
-    //     console.log('userService:', userService);
-    //     // expect(userServiceLogInSpy).toHaveBeenCalled();
-    //     expect(platformReadySpy).toHaveBeenCalled();
-    //     expect(statusBarSpy.styleDefault).toHaveBeenCalled();
-    //     expect(splashScreenSpy.hide).toHaveBeenCalled();
-    // })));
+    it('should authenticate and initialize', async () => {
+        const userService = TestBed.inject(UserService);
+        const platform = TestBed.inject(Platform);
+        const statusBar = TestBed.inject(StatusBar);
+        const splashScreen = TestBed.inject(SplashScreen);
 
+        const userServiceLogInSpy = spyOn(userService, 'logIn').and.callThrough();
+        const platformReadySpy = spyOn(platform, 'ready').and.callThrough();
+        const statusBarStyleDefaultSpy = spyOn(statusBar, 'styleDefault');
+        const splashScreenHideSpy = spyOn(splashScreen, 'hide');
 
+        const fixture = TestBed.createComponent(AppComponent);
+        const component: AppComponent = fixture.debugElement.componentInstance;
+        fixture.detectChanges();
+
+        expect(component).toBeTruthy();
+        expect(component.isReady).toBe(false);
+        expect(userServiceLogInSpy).toHaveBeenCalled();
+        await expectAsync(userService.logIn()).toBeRejected();
+        expect(platformReadySpy).toHaveBeenCalled();
+        await expectAsync(platform.ready()).toBeResolved();
+        expect(statusBarStyleDefaultSpy).toHaveBeenCalled();
+        expect(splashScreenHideSpy).toHaveBeenCalled();
+        expect(component.isReady).toBe(true);
+    });
 
     // it('should have menu labels', async () => {
     //     const fixture = await TestBed.createComponent(AppComponent);
