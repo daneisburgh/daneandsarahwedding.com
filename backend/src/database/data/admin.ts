de2558306eab31c0e12245dcda9572626b6adfb8
Dane Isburgh <daneisburgh@gmail.com>
2020-04-26 17:32:11 -0400

    Add email to admin creation

--- backend/src/database/data/admin.ts
+++ backend/src/database/data/admin.ts
@@ -4,6 +4,7 @@ export = async function () {
     await User.create({
         username: 'D&S',
         name: 'Dane & Sarah Isburgh',
+        email: `hello@${process.env.DOMAIN_NAME}`,
         address: '7907 Ramble View #304, Cincinnati OH, 45231',
         maxGuests: 2,
         isAdmin: true
