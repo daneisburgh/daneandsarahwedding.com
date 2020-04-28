df6306ea6d91f41c359621cffc502bf61447db5e
daneisburgh <daneisburgh@gmail.com>
2020-04-28 08:47:50 -0400

    Update info page content

--- frontend/src/app/pages/info/info.page.ts
+++ frontend/src/app/pages/info/info.page.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
+import { deadlineString } from '../profile/profile.page';
+import { LogInModalComponent } from '../../components/log-in-modal/log-in-modal.component';
+import { UserService } from '../../services/user/user.service';
 import { UtilsService } from '../../services/utils/utils.service';
 
 @Component({
@@ -8,11 +12,21 @@ import { UtilsService } from '../../services/utils/utils.service';
     styleUrls: ['./info.page.scss'],
 })
 export class InfoPage {
+    public deadlineString = deadlineString;
+
+    public get user() { return this.userService.user; }
+
     constructor(
+        public modalController: ModalController,
+        private userService: UserService,
         private utilsService: UtilsService
     ) { }
 
     public ionViewDidEnter() {
         this.utilsService.setTitle('Info');
+    }
+
+    public async presentLogInModal() {
+        (await this.modalController.create({ component: LogInModalComponent })).present();
     }
 }
