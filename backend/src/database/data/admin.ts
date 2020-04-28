f40716a99baac62d45fb470bda7eb6533fa4a384
daneisburgh <daneisburgh@gmail.com>
2020-04-27 22:25:33 -0400

    Fix admin email

--- backend/src/database/data/admin.ts
+++ backend/src/database/data/admin.ts
@@ -4,7 +4,7 @@ export = async function () {
     await User.create({
         username: 'D&S',
         name: 'Dane & Sarah Isburgh',
-        email: `hello@${process.env.DOMAIN_NAME}`,
+        email: `hello@${process.env.APP_NAME}.com`,
         address: '7907 Ramble View #304, Cincinnati OH, 45231',
         maxGuests: 2,
         isAdmin: true
