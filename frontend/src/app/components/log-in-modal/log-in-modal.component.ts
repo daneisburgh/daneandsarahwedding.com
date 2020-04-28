80fda68a03988c6ff51cd550d8ba6dbf3d0590f3
daneisburgh <daneisburgh@gmail.com>
2020-04-28 08:49:31 -0400

    Update log in modal to present email reset

--- frontend/src/app/components/log-in-modal/log-in-modal.component.ts
+++ frontend/src/app/components/log-in-modal/log-in-modal.component.ts
@@ -4,6 +4,7 @@ import { ModalController } from '@ionic/angular';
 
 import { UserService } from '../../services/user/user.service';
 import { UtilsService } from '../../services/utils/utils.service';
+import { EmailResetModalComponent } from '../email-reset-modal/email-reset-modal.component';
 
 const loginErrors = [
     'Invalid Username',
@@ -23,17 +24,22 @@ export class LogInModalComponent {
     public isSubmitting = false;
     public hasError = false;
 
+    public get user() { return this.userService.user; }
     public get isMobile() { return this.utilsService.isMobile; }
+
+    private modal: HTMLIonModalElement;
 
     constructor(
         private modalController: ModalController,
         private router: Router,
         private userService: UserService,
         private utilsService: UtilsService
-    ) { }
+    ) {
+        this.getModal();
+    }
 
     public dismiss() {
-        this.modalController.dismiss();
+        this.modal.dismiss();
     }
 
     public async submit() {
@@ -42,9 +48,17 @@ export class LogInModalComponent {
 
         try {
             await this.userService.logIn({ username: this.username, password: this.password });
-            const route = this.userService.user.isAdmin ? '/users' : '/profile';
-            await this.router.navigate([route]);
-            this.dismiss();
+
+            if (this.user.email) {
+                const route = this.user.isAdmin ? '/users' : '/profile';
+                await this.router.navigate([route]);
+            } else {
+                await this.utilsService.presentToast('danger', 'Please add an email to continue');
+                const modal = await this.modalController.create({ component: EmailResetModalComponent });
+                await modal.present();
+                await modal.onWillDismiss();
+                this.dismiss();
+            }
         } catch (error) {
             this.hasError = true;
             this.errorMessage = loginErrors.includes(error.error) ? error.error : 'Bad Request';
@@ -64,4 +78,8 @@ export class LogInModalComponent {
     }
 
     public presentResetPasswordModal() { }
+
+    private async getModal() {
+        this.modal = await this.modalController.getTop();
+    }
 }
