87e56f5c68d68b12bee57d5943aff501a87b50f8
daneisburgh <daneisburgh@gmail.com>
2020-04-29 23:51:23 -0400

    Refactor login route

--- backend/src/api/routes/login.ts
+++ backend/src/api/routes/login.ts
@@ -1,53 +1,50 @@
 import bcrypt from 'bcryptjs';
 import { pick } from 'lodash';
 
-import { createResponse, getRequestBody, createToken, verifyToken } from '../utils';
+import { createResponse, getRequestBody, createTokenFromUsername, getUserFromToken } from '../utils';
 import User from '../../database/models/user';
 
 export default async function (event: any) {
     try {
         const body = getRequestBody(event);
-        const { password, token } = body;
-        let { username } = body;
+        const { token, username, password } = body;
+        let user: User | null;
 
         if (token) {
             try {
-                const decoded = verifyToken(token);
-                username = decoded.username;
+                user = await getUserFromToken(token);
             } catch {
                 return createResponse(401, 'Invalid Token');
             }
+        } else {
+            user = await User.findByPk(username);
         }
-
-        const user = await User.findOne({ where: { username: (username ? username : '') } });
 
         if (!user) {
             return createResponse(401, 'Invalid Username');
-        }
-
-        if (!token && (
+        } else if (!token && (
             (!user.isPasswordHashed && password !== user.password) ||
             (user.isPasswordHashed && !bcrypt.compareSync(password as string, user.password))
         )) {
             return createResponse(401, 'Invalid Password');
+        } else {
+            return createResponse(200, {
+                token: createTokenFromUsername(user.username),
+                user: pick(user, [
+                    'username',
+                    'name',
+                    'email',
+                    'isEmailConfirmed',
+                    'guests',
+                    'maxGuests',
+                    'isAdmin',
+                    'isAttending',
+                    'requiresAccommodations',
+                    'totalRequiredRooms',
+                    'requiresTransportation'
+                ])
+            });
         }
-
-        return createResponse(200, {
-            token: createToken(username),
-            user: pick(user, [
-                'username',
-                'name',
-                'email',
-                'isEmailConfirmed',
-                'guests',
-                'maxGuests',
-                'isAdmin',
-                'isAttending',
-                'requiresAccommodations',
-                'totalRequiredRooms',
-                'requiresTransportation'
-            ])
-        });
     } catch (error) {
         console.error(error);
         return createResponse(500);
