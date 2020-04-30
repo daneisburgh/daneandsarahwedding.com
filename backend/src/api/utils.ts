a9b61415775ce0bd65ae6131cde1c761270b311e
daneisburgh <daneisburgh@gmail.com>
2020-04-29 23:51:59 -0400

    Refactor route utils, add token helpers

--- backend/src/api/utils.ts
+++ backend/src/api/utils.ts
@@ -1,13 +1,8 @@
 import jwt from 'jsonwebtoken';
 import { get } from 'lodash';
+import User from '../database/models/user';
 
 const API_KEY = process.env.API_KEY as string;
-
-interface Decoded {
-    username: string;
-    iat: number;
-    exp: number;
-}
 
 export function getRequestBody(event: object) {
     return JSON.parse(get(event, 'body'));
@@ -17,10 +12,32 @@ export function createResponse(statusCode: number, body?: any) {
     return { statusCode, body: JSON.stringify(body) };
 }
 
-export function createToken(username: string) {
+export function createTokenFromUsername(username: string) {
     return jwt.sign({ username }, API_KEY, { expiresIn: '1 week' });
 }
 
-export function verifyToken(token: string) {
-    return jwt.verify(token, API_KEY) as Decoded;
+export async function getUserFromToken(token: string) {
+    const user = await User.findByPk((jwt.verify(token, API_KEY) as any).username);
+
+    if (!user) {
+        throw new Error();
+    }
+
+    return user;
+}
+
+export async function getRandomToken(tokenKey: string): Promise<string> {
+    const token = Math.random().toString(36).substring(2);
+
+    if (await User.findOne({ where: { [tokenKey]: token } })) {
+        return await getRandomToken(tokenKey);
+    } else {
+        return token;
+    }
+}
+
+export function getTokenExpiration(days: number) {
+    const date = new Date();
+    date.setDate(date.getDate() + days);
+    return date;
 }
