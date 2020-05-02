bb2a848e8ef1ef19491e4f8fc8fbf4f08766830e
daneisburgh <daneisburgh@gmail.com>
2020-05-02 12:49:36 -0400

    Update database name to be node environment

--- backend/src/database/config.ts
+++ backend/src/database/config.ts
@@ -1,7 +1,7 @@
 export = {
     [process.env.NODE_ENV as string]: {
         dialect: 'postgres',
-        database: 'postgres',
+        database: process.env.NODE_ENV,
         host: process.env.DB_HOST as string,
         username: process.env.DB_USERNAME as string,
         password: process.env.DB_PASSWORD as string
