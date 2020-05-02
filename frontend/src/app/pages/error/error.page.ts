8809468cbceaa624f28482476935808a6e832c36
daneisburgh <daneisburgh@gmail.com>
2020-05-02 13:47:19 -0400

    Update error page status message and title

--- frontend/src/app/pages/error/error.page.ts
+++ frontend/src/app/pages/error/error.page.ts
@@ -13,7 +13,7 @@ import { UtilsService } from '../../services/utils/utils.service';
 export class ErrorPage {
     public statusCode: number;
     public get statusText() { return get(builtinStatusCodes, this.statusCode); }
-    public get statusMessage() { return this.statusCode ? `${this.statusCode}: ${this.statusText}` : ''; }
+    public get statusMessage() { return this.statusCode ? `Error: ${this.statusText}` : ''; }
 
     constructor(
         private activatedRoute: ActivatedRoute,
@@ -21,8 +21,8 @@ export class ErrorPage {
     ) { }
 
     public ionViewDidEnter() {
+        this.utilsService.setTitle('Error');
         const params = this.activatedRoute.snapshot.params;
         this.statusCode = params.statusCode && get(builtinStatusCodes, params.statusCode) ? params.statusCode : 404;
-        this.utilsService.setTitle(this.statusText);
     }
 }
