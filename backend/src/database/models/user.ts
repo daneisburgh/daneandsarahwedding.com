14f573761fd5ed310e99efc4f8eca2908cb1c197
daneisburgh <daneisburgh@gmail.com>
2020-05-03 01:39:44 -0400

    Change password reset to password change

--- backend/src/database/models/user.ts
+++ backend/src/database/models/user.ts
@@ -27,12 +27,12 @@ export const userColumns = {
         allowNull: false,
         defaultValue: false
     },
-    passwordResetCode: {
+    passwordChangeCode: {
         type: DataTypes.STRING,
         allowNull: true,
         unique: true
     },
-    passwordResetExpiration: {
+    passwordChangeExpiration: {
         type: DataTypes.DATE,
         allowNull: true
     },
@@ -100,8 +100,8 @@ class User extends Model {
     public address!: string;
     public password!: string;
     public isPasswordHashed!: boolean;
-    public passwordResetCode!: string;
-    public passwordResetExpiration!: Date;
+    public passwordChangeCode!: string;
+    public passwordChangeExpiration!: Date;
     public email!: string;
     public emailVerificationCode!: string;
     public emailVerificationExpiration!: Date;
