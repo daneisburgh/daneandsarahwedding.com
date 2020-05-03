464c7564841c5af4bdbe8987164be9529a54a6dd
daneisburgh <daneisburgh@gmail.com>
2020-05-03 01:40:30 -0400

    Add change password email route

---
+++ backend/src/api/routes/user-password-email.ts
@@ -0,0 +1,35 @@
+import { getRequestBody, getUserFromToken, createResponse, createUserResponse, createCodeAndExpiration } from '../utils';
+import User from '../../database/models/user';
+import sendEmail from '../../email';
+
+export default async function (event: any) {
+    try {
+        const { email } = getRequestBody(event);
+        const user = await User.findOne({ where: { email } });
+
+        if (!user) {
+            return createResponse(400, 'Invalid email');
+        } else if (!user.isEmailVerified) {
+            return createResponse(400, 'Email not verified');
+        } else {
+            const expirationHours = 2;
+            const { code, expiration } = await createCodeAndExpiration('passwordChangeCode', expirationHours);
+
+            await user.update({
+                passwordChangeCode: code,
+                passwordChangeExpiration: expiration
+            });
+
+            await sendEmail('password-change', user, {
+                expirationHours,
+                verificationUrl: `${process.env.CLIENT_URL}?passwordChangeCode=${code}`
+            });
+
+            return createResponse(200, createUserResponse(user));
+        }
+    } catch (error) {
+        console.error(error);
+        return createResponse(500);
+    }
+}
+
