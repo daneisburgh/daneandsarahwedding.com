676265edfb0ab039cdf98031a9f171157bbde58b
Dane Isburgh <daneisburgh@gmail.com>
2020-05-09 10:13:35 -0400

    Update run script build/deploy command

--- run.sh
+++ run.sh
@@ -6,7 +6,6 @@
 #   2) start-dev ---------------- Start development environment
 #   3) lint --------------------- Run backend and frontend linters
 #   4) test --------------------- Run backend and frontend tests
-#   5) build -------------------- Build build backend and frontend apps
 #   5) deploy ENV --------------- Deploy app to given environment
 #   6) destroy ENV -------------- Destroy app in given environment
 #
@@ -21,8 +20,8 @@ function validate_environment {
         echo "Invalid environment"
         exit
     else
-        source vars/default.conf
-        source vars/$1.conf
+        source keys/default.conf
+        source keys/$1.conf
     fi
 }
 
@@ -39,7 +38,7 @@ elif [ $1 == "install-dependencies" ]; then
 elif [ $1 == "start-dev" ]; then
     echo "Starting dev environment..."
     validate_environment "dev"
-    concurrently -k -n "DATABASE,BACKEND,FRONTEND" -c "blue,green,red" \
+    concurrently -n "DATABASE,BACKEND,FRONTEND" -c "blue,green,red" \
         "docker-compose --file backend/docker-compose.yml up" \
         "npm --prefix backend run serve" \
         "npm --prefix frontend run serve"
@@ -52,24 +51,18 @@ elif [ $1 == "lint" ]; then
 elif [ $1 == "test" ]; then
     echo "Running frontend tests ..."
     npm --prefix frontend run test
-elif [ $1 == "build" ]; then
-    validate_environment $2
-    echo "Building $2 app..."
-    npm --prefix backend build
-    npm --prefix frontend run build:$NODE_ENV
-    mkdir -p backend/build/public
-    rm -rf backend/build/public/www
-    cp -R frontend/www backend/build/public/www
 elif [ $1 == "deploy" ]; then
     validate_environment $2
     echo "Deploying $2 app..."
-    ./run.sh build $2
+    npm --prefix backend build
+    npm --prefix frontend run build:$NODE_ENV
+    cp -R frontend/www backend/build/public/www
     npm --prefix backend run deploy
 elif [ $1 == "destroy" ]; then
     validate_environment $2
     echo "Destroying $2 app..."
     npm --prefix backend run destroy
 else
-    echo "Invalid command argument"
+    echo "Uknown command"
     exit
 fi
