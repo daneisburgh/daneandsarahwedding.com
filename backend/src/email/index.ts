dc7b3f10575e7fdeb09e715d38ebbdc9ba6120b4
Dane Isburgh <daneisburgh@gmail.com>
2020-05-11 17:30:06 -0400

    Refactor email to use aws ses transport

--- backend/src/email/index.ts
+++ backend/src/email/index.ts
@@ -1,3 +1,4 @@
+import aws from 'aws-sdk';
 import EmailTemplate from 'email-templates';
 import nodemailer from 'nodemailer';
 import path from 'path';
@@ -5,32 +6,25 @@ import path from 'path';
 import Email from '../database/models/email';
 import User from '../database/models/user';
 
-const { APP_NAME, NODE_ENV, SMTP_USERNAME, SMTP_PASSWORD } = process.env;
+const { APP_NAME, NODE_ENV } = process.env;
 const sendEmail = NODE_ENV !== 'development';
-
-const transport = nodemailer.createTransport({
-    host: 'email-smtp.us-east-1.amazonaws.com',
-    port: 587,
-    auth: {
-        user: SMTP_USERNAME,
-        pass: SMTP_PASSWORD
-    }
-});
-
-const emailTemplate = new EmailTemplate({
-    message: { from: `"Dane & Sarah" hello@${APP_NAME}.com` },
-    transport,
-    juice: true,
-    juiceResources: {
-        preserveImportant: true,
-        webResources: { relativeTo: path.resolve(__dirname, 'emails') }
-    },
-    preview: !sendEmail,
-    send: sendEmail
-});
 
 export default async function (template: string, user: User, params: object) {
     try {
+        const emailTemplate = new EmailTemplate({
+            message: { from: `"Dane & Sarah" hello@${APP_NAME}.com` },
+            transport: nodemailer.createTransport({
+                SES: new aws.SES({ apiVersion: '2010-12-01' })
+            }),
+            juice: true,
+            juiceResources: {
+                preserveImportant: true,
+                webResources: { relativeTo: path.resolve(__dirname, 'emails') }
+            },
+            preview: !sendEmail,
+            send: sendEmail
+        });
+
         const result = await emailTemplate.send({
             template: path.resolve(__dirname, 'emails', template),
             message: { to: user.email },
