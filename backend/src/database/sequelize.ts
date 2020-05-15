96a2afc23ee70ade9e1dad4fd929a162ffd5ab49
Dane Isburgh <daneisburgh@gmail.com>
2020-05-15 19:06:27 -0400

    Update database name and version

--- backend/src/database/sequelize.ts
+++ backend/src/database/sequelize.ts
@@ -1,12 +1,12 @@
 import { Sequelize } from 'sequelize';
 
-const { NODE_ENV, DB_USERNAME, DB_PASSWORD, DB_HOST, TUNNEL } = process.env;
+const { APP_NAME, NODE_ENV, DB_USERNAME, DB_PASSWORD, DB_HOST, TUNNEL } = process.env;
 
 export default new Sequelize({
     dialect: 'postgres',
     host: NODE_ENV === 'development' || TUNNEL === 'true' ? 'localhost' : DB_HOST,
     port: 5432,
-    database: NODE_ENV,
+    database: `${APP_NAME}-${NODE_ENV}`,
     username: DB_USERNAME,
     password: DB_PASSWORD
 });
