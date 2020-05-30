7be1cbf945328fd6fe8d662a4b2fe3b7fe654d37
Dane Isburgh <daneisburgh@gmail.com>
2020-05-30 12:26:13 -0400

    refactor(profile): guest name message

--- frontend/src/app/pages/profile/profile.page.ts
+++ frontend/src/app/pages/profile/profile.page.ts
@@ -16,7 +16,7 @@ export const deadlineString = deadline.toLocaleDateString();
 })
 export class ProfilePage {
     public readonly deadlineMessage = `Profile info can be updated until ${deadlineString}`;
-    public readonly guestNameMessage = 'Please include guest first and last name';
+    public readonly guestNameMessage = 'Please include guest\'s first and last name';
 
     public email: string;
     public guests: string[];
